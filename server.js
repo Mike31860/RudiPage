0ee6ad71d4bfb183bfeb2ea5bd23809035ccfe4f
Mike31860 <miguel980429@gmail.com>
2021-05-18 19:17:58 -0500

    heroku changes

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.static(__dirname + '/dist/rudi-page'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(`${__dirname}/src/index.js`));
+res.sendFile(path.join(`${__dirname}/public/index.html`));
 });
 
 app.listen(process.env.PORT || 8080);
